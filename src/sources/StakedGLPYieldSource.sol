f406bb20b59ea16bf453e71524d13141b7c7765e
ortutay <marcell.ortutay@gmail.com>
2023-05-09 15:51:40 -0700

    BSL

--- src/sources/StakedGLPYieldSource.sol
+++ src/sources/StakedGLPYieldSource.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BSL
 pragma solidity ^0.8.13;
 
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
